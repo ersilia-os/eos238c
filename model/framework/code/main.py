b3dd3e07dbb8f89d11c2a0c37c832a104ec4198e
Amna Ali <98670206+Amna-28@users.noreply.github.com>
2022-10-18 07:17:51 +0200

    Update main.py

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -12,7 +12,7 @@ output_file = sys.argv[2]
 root = os.path.dirname(os.path.abspath(__file__))
 
 # model directory
-model_dir = os.path.abspath(os.path.join(root, "..", "model"))
+model_dir = os.path.abspath(os.path.join(root, "..", "models"))
 
 # parse arguments
 input_file = sys.argv[1]
